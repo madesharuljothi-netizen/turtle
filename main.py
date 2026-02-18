93d9977c2d27b8bc46e482fbdd5589de7f625637
madesharuljothi-netizen <madesh.aruljothi@gmail.com>
2026-02-18 17:49:23 +0000

    turtleHM

--- main.py
+++ main.py
@@ -14,6 +14,33 @@ for i in range(10):
      pen.forward(100)
      pen.right(90)
        
+
+  
+turtle.Screen().bgcolor("Orange")
+  
+sc = turtle.Screen()
+sc.setup(500, 400)
+  
+turtle.title("Welcome to the turtle window")
+  
+pen = turtle.Turtle()
+  
+for i in range(10):
+     pen.forward(100)
+     pen.right(60)
        
        
-       
+
+  
+turtle.Screen().bgcolor("Orange")
+  
+sc = turtle.Screen()
+sc.setup(500, 400)
+  
+turtle.title("Welcome to the turtle window")
+  
+pen = turtle.Turtle()
+  
+for i in range(10):
+     pen.forward(100)
+     pen.right(120)
