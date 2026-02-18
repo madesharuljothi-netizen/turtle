ba22b72f048dfae8987c395e83f7c47d5cff57bd
madesharuljothi-netizen <madesh.aruljothi@gmail.com>
2026-02-18 17:43:11 +0000

    turtle

---
+++ main.py
@@ -0,0 +1,19 @@
+  
+import turtle
+  
+turtle.Screen().bgcolor("Orange")
+  
+sc = turtle.Screen()
+sc.setup(500, 400)
+  
+turtle.title("Welcome to the turtle window")
+  
+pen = turtle.Turtle()
+  
+for i in range(10):
+     pen.forward(100)
+     pen.right(90)
+       
+       
+       
+       
